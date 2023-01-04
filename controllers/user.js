ff19e449ec1feaf935c4c17e1e145c65ea9c1d99
Edwin1403 <109071573+Edwin1403@users.noreply.github.com>
2023-01-04 16:02:41 +0530

    commit

--- controllers/user.js
+++ controllers/user.js
@@ -1,35 +1,93 @@
 const User = require('../Model/User');
-const { validateEmail } = require('../validators/validation');
+const { validateEmail, validateLength, validateUsername } = require('../validators/validation');
 
-
+const bcrypt = require('bcrypt');
+const { generateToken } = require('../token/token');
 
 exports.home = (req, res) => {
     res.status(200).json({
-        Message : "Home page is Welcomes You..!",
+        Message: "Home page is Welcomes You..!",
         Success: true,
     });
 };
 
 exports.register = async (req, res) => {
     try {
+        const {
+            first_name,
+            last_name,
+            username,
+            email,
+            password,
+            bYear,
+            bMonth,
+            bDay,
+            gender,
+        } = req.body;
+
+        if (!validateEmail(email)) {
+            return res.status(400).json({
+                Success: false,
+                Message: "Email is not valid",
+            });
+        }
+        const checkEmail = await User.findOne({ email });
+        if (checkEmail) {
+            return res.status(400).json({
+                message: "Email is already in use, Try another one",
+            });
+        }
+
+        if (!validateLength(first_name, 3, 30)) {
+            return res.status(400).json({
+                message: "First Name must be between 3 and 30 characters",
+            });
+        }
+        if (!validateLength(last_name, 3, 30)) {
+            return res.status(401).json({
+                message: "Last Name must be between 3 and 30 characters",
+            })
+        };
+        if (!validateLength(password, 6, 20)) {
+            return res.status(402).json({
+                message: "Password must be between 6 and 20 characters",
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+        let tempUserName = first_name + last_name;
+        let newUsername = await validateUsername(tempUserName);
+
         const user = new User(
-           req.body
-        ).save();
-            if (!validateEmail(req.body.email)) {
-                return res.status(400).json({
-                    Message : "Please enter a valid email address",
-                    Success: false,
-                });
+            {
+                first_name,
+                last_name,
+                username: newUsername,
+                email,
+                password: hashedPassword,
+                bYear,
+                bMonth,
+                bDay,
+                gender,
             }
+        ).save();
+        
+        const emailVerificationToken = generateToken(
+            { id: user._id.toString() },
+            "30m"
+        );
+        console.log(emailVerificationToken);
+
         res.status(200).json({
-            Message : "Register Success",
+            Message: "User created successfully",
             Success: true,
         });
+
     } catch (error) {
         res.status(500).json({
-            Message:error.message,
+            Message: error.message,
             Success: false,
         })
     }
-    
 };
--- validators/validation.js
+++ validators/validation.js
@@ -3,3 +3,32 @@ exports.validateEmail = (email) => {
         .toLowerCase()
         .match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)
 };
+exports.validateLength = (text, min, max) => {
+    if (text.length < min || text.length > max) {
+        return false;
+    }
+    return true;
+};
+
+exports.validateUsername = async (username) => {
+    let a = false;
+
+    do {
+        let check = await User.findOne({ username });
+        if (check) {
+            // change username
+            username += (+new Date() * Math.random()).toString().substring(0, 1)
+            a = true;
+        } else {
+            a = false;
+        }
+    } while (a) {
+        return username;
+    }
+
+}
+
+// exports.validatePassword = (password) => {
+//     return String(password)
+//     .match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{4,8}$/)
+// }
